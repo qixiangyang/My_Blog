86743134fc23e0eadefa4697d5a0bb088a9fabd5
qixiangyang <qixiangyangrm@foxmail.com>
2019-09-17 17:16:02 +0800

    新增about.html页面 修改base.html 新增meta数据 新增404, 500 等错误界面

--- app.py
+++ app.py
@@ -10,7 +10,20 @@ def hello_world():
     return render_template('index.html')
 
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def server_internal_error(e):
+    return render_template('500.html'), 500
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-
-
