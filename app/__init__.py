ad41fbd588fa8e66f4e1eb22f619499e17f3a2f1
qixiangyang <qixiangyangrm@foxmail.com>
2019-09-18 17:25:17 +0800

    调整Flask 开启方式

--- app/__init__.py
+++ app/__init__.py
@@ -7,16 +7,19 @@ File: __init__
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
-
+from config import config
+ 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 
 
 def create_app(config_name):
     app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
     bootstrap.init_app(app)
 
     from .blog import blog as blog_blueprint
-    app.register_blueprint(blog_blueprint
-    )
+    app.register_blueprint(blog_blueprint)
     return app
--- blog_start.py
+++ blog_start.py
@@ -1,10 +1,6 @@
 import os
-import click
 from flask_migrate import Migrate
 from app import create_app, db
 
-app = create_app('default')
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
-
-
-
