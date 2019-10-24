4040ccdba4bf1126fc2cc9304a666078aef1ff2d
qixiangyang <qixiangyangrm@foxmail.com>
2019-10-24 17:37:10 +0800

    用户登陆跑通

--- app/auth/views.py
+++ app/auth/views.py
@@ -15,15 +15,17 @@ from .forms import LoginForm
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    print(form)
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data.lower()).first()
+        print(user)
         if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
             next = request.args.get('next')
             if next is None or not next.startswith('/'):
-                next = url_for('blog.pyhub')
+                next = url_for('blog.py_news')
             return redirect(next)
-        flash('Invalid email or password.')
+    flash('Invalid email or password.')
     return render_template('auth/login.html', form=form)
 
 
