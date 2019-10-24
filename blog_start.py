be1d083d0ac267973fa19ff2425bd6f2c007341b
qixiangyang <qixiangyangrm@foxmail.com>
2019-10-24 17:38:35 +0800

    更新数据库模型，可通过shell访问

--- blog_start.py
+++ blog_start.py
@@ -1,7 +1,7 @@
 import os
 from flask_migrate import Migrate
 from app import create_app, db
-from app.models import Article, PyNews
+from app.models import Article, PyNews, User, Role
 
 # print(os.getenv('FLASK_ENVIRONMENT'))
 app = create_app(os.getenv('FLASK_ENV') or 'default')
@@ -10,4 +10,4 @@ migrate = Migrate(app, db)
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(db=db, Article=Article, PyNews=PyNews)
+    return dict(db=db, Article=Article, PyNews=PyNews, User=User, Role=Role)
