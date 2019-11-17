5833dd4b0ec5e9b8edd19b4fd6373611c5d7be10
qixiangyang <qixiangyangrm@foxmail.com>
2019-11-17 14:33:48 +0800

    Blog V2.1.4 23 新增存草稿表单字段

--- app/blog/forms.py
+++ app/blog/forms.py
@@ -7,14 +7,15 @@ File: forms
 
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField
+from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 
 class PostForm(FlaskForm):
     title = StringField('标题', [DataRequired(), Length(max=255)])
     text = TextAreaField('内容', [DataRequired()])
-    text_pre = TextAreaField('预览', [DataRequired()])
+    text_pre = TextAreaField('内容概要', [DataRequired()])
     category = StringField('分类', [DataRequired(), Length(max=255)])
     tags = StringField('标签', [DataRequired(), Length(max=255)])
+    save = SubmitField('存草稿')
 
