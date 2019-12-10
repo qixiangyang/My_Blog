0ff956b1837cc5be7e65bb20356229784e813a60
qixiangyang <qixiangyangrm@foxmail.com>
2019-12-10 11:30:02 +0800

    Blog V2.1.5 21. 新增日志

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 from flask_login import LoginManager
 from .filters import handle_time
+import logging
+from logging.handlers import TimedRotatingFileHandler
 
 from config import config
 
@@ -20,12 +22,21 @@ login_manager.login_view = 'auth.login'
 
 
 def create_app(config_name):
+
     app = Flask(__name__)
     app.config.from_object(config[config_name])
 
     mail.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+
+    formatter = logging.Formatter(
+        "[%(asctime)s][%(filename)s:%(lineno)d][%(levelname)s][%(thread)d] - %(message)s")
+    handler = TimedRotatingFileHandler(
+        "log/flask.log", when="D", interval=1, backupCount=15,
+        encoding="UTF-8", delay=False, utc=True)
+    app.logger.addHandler(handler)
+    handler.setFormatter(formatter)
 
     from .blog import blog as blog_blueprint
     app.register_blueprint(blog_blueprint)
