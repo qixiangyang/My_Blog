b4967e8a4f87e7302b3172611d8f18a57a95eaf4
qixiangyang <qixiangyangrm@foxmail.com>
2021-01-12 14:44:07 +0800

    add gunicorn config

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,11 +1,11 @@
 from gunicorn import glogging
 glogging.Logger.error_fmt = "%(asctime)s [%(filename)s:%(lineno)d] [%(funcName)s] " \
                             "[%(levelname)s] [pid: %(process)d thread_id:%(thread)d] - %(message)s"
-workers = 5  # 并行工作进程数
+workers = 3  # 并行工作进程数
 threads = 2  # 指定每个工作者的线程数
 bind = ['0.0.0.0:5000']  # 监听内网端口5000
 worker_connections = 1000  # 设置最大并发量
-proc_name = 'acenter'  # 进程名称
+proc_name = 'my_blog'  # 进程名称
 pidfile = '/tmp/blog.pid'  # 设置进程文件目录
 timeout = 60  # 超时
 max_requests = 6000  # 最大请求数
