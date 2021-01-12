c270d85eed300a5e2a00d6937e2a0ff714f391a8
qixiangyang <qixiangyangrm@foxmail.com>
2021-01-12 14:42:50 +0800

    update script file

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 source venv/bin/activate
-export FLASK_APP=blog_start.py
+export FLASK_APP=my_blog.py
 source ~/.bash_profile
+#flask db upgrade
 #flask run --port=5555 --host=192.168.8.239
 flask run --port=8008 --host=0.0.0.0
+#gunicorn my_blog:app -c gunicorn_config.py
