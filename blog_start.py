1594f600bb225b9b19c6fb5dddf613d02f1cfd5d
qixiangyang <qixiangyangrm@foxmail.com>
2021-01-12 14:53:43 +0800

    update file

--- blog_start.py
+++
@@ -1,9 +0,0 @@
-import os
-from flask_migrate import Migrate
-from app import create_app, db
-
-
-# print(os.getenv('FLASK_ENVIRONMENT'))
-app = create_app(os.getenv('FLASK_ENV') or 'default')
-migrate = Migrate(app, db)
-
