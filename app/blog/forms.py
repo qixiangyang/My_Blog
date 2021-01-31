f4b4c3bd05042fdffb91f4920b056d6715a90108
qixiangyang <qixiangyangrm@foxmail.com>
2021-01-31 17:17:57 +0800

    update form config

--- app/blog/forms.py
+++ app/blog/forms.py
@@ -5,19 +5,19 @@ Date: 2019/10/27 3:07 下午
 File: forms 
 """
 
-
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
+from wtforms import StringField, TextAreaField, SubmitField, SelectMultipleField, SelectField
 from wtforms.validators import DataRequired, Length
 
 
 class PostForm(FlaskForm):
+
     title = StringField('标题', [DataRequired(), Length(max=255)])
     text = TextAreaField('内容', [DataRequired()])
     text_pre = TextAreaField('内容概要', [DataRequired()])
-    category = StringField('分类', [DataRequired(), Length(max=255)])
     tags = StringField('标签', [DataRequired(), Length(max=255)])
-    save = SubmitField('存草稿')
+    category = SelectField('分类', [DataRequired()])
+    save_script = SubmitField('存草稿')
 
 
 class SourceForm(FlaskForm):
