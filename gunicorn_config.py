ee341ad773021279b036d4219909e2c52f7b6bc3
xiangyang.qi <xiangyang.qi@advance.ai>
2021-04-16 13:10:34 +0800

    update log format

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,6 +1,37 @@
 from gunicorn import glogging
-glogging.Logger.error_fmt = "%(asctime)s [%(filename)s:%(lineno)d] [%(funcName)s] " \
-                            "[%(levelname)s] [pid: %(process)d thread_id:%(thread)d] - %(message)s"
+
+# 日志格式说明
+"""
+asctime	%(asctime)s	日志事件发生的时间--人类可读时间，如：2003-07-08 16:49:45,896
+created	%(created)f	日志事件发生的时间--时间戳，就是当时调用time.time()函数返回的值
+relativeCreated	%(relativeCreated)d	日志事件发生的时间相对于logging模块加载时间的相对毫秒数（目前还不知道干嘛用的）
+msecs	%(msecs)d	日志事件发生事件的毫秒部分
+levelname	%(levelname)s	该日志记录的文字形式的日志级别（'DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'）
+levelno	%(levelno)s	该日志记录的数字形式的日志级别（10, 20, 30, 40, 50）
+name	%(name)s	所使用的日志器名称，默认是'root'，因为默认使用的是 rootLogger
+message	%(message)s	日志记录的文本内容，通过 msg % args计算得到的
+pathname	%(pathname)s	调用日志记录函数的源码文件的全路径
+filename	%(filename)s	pathname的文件名部分，包含文件后缀
+module	%(module)s	filename的名称部分，不包含后缀
+lineno	%(lineno)d	调用日志记录函数的源代码所在的行号
+funcName	%(funcName)s	调用日志记录函数的函数名
+process	%(process)d	进程ID
+processName	%(processName)s	进程名称，Python 3.1新增
+thread	%(thread)d	线程ID
+threadName	%(thread)s	线程名称
+"""
+glogging.Logger.error_fmt = '{' \
+                            '"time": "%(asctime)s", ' \
+                            '"log_level": "%(levelname)s", ' \
+                            '"pathname": "%(pathname)s", ' \
+                            '"filename": "%(filename)s", ' \
+                            '"lineno": "%(lineno)s", ' \
+                            '"module_name":"%(module)s",  ' \
+                            '"method": "%(funcName)s",  ' \
+                            '"process_id":%(process)d,  ' \
+                            '"thread_id":%(thread)d,  ' \
+                            '"message": "%(message)s"' \
+                            '}'
 workers = 3  # 并行工作进程数
 threads = 2  # 指定每个工作者的线程数
 bind = ['0.0.0.0:5000']  # 监听内网端口5000
--- my_blog.py
+++ my_blog.py
@@ -5,11 +5,12 @@ from app import create_app, db
 
 app = create_app(os.getenv('FLASK_ENV') or 'default')
 migrate = Migrate(app, db)
+print(os.getenv('FLASK_ENV'))
 
 
 if __name__ != '__main__':
     # 如果不是直接运行，则将日志输出到 gunicorn 中
-    if app.config.get("DEBUG") is False:
+    if app.config.get("PRODUCT"):
         gunicorn_logger = logging.getLogger('gunicorn.error')
         app.logger.handlers = gunicorn_logger.handlers
         app.logger.setLevel(gunicorn_logger.level)
